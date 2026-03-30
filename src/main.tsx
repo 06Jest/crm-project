68d3dc04577a1e029a1b873a94597472c33f5da4
jest <juuzouxken@gmail.com>
2026-03-30 08:10:13 +0800

    Complete initial Contact Page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
-import { ThemeProvider } from "@mui/material/styles";
-import theme from './theme';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import App from './App';
+import theme from './theme';
 
-<ThemeProvider theme = {theme}>
-  <App />
-</ThemeProvider>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
