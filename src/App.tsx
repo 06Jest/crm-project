7381953f9399e501106e59c458a4c88453563241
Jestony Silvano <silvanojestony27@gmail.com>
2026-03-30 15:38:04 +0800

    Skeleton folder structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard/Dashboard';
-import MainLayout from './layout/Mainlayout';
-import Contacts from './pages/Contacts/Contacts';
-import Leads from './pages/Leads/Leads';
-import Settings from './pages/Settings';
+import Dashboard from './pages/app/Dashboard/Dashboard';
+import AppLayout from './layout/Applayout';
+import Contacts from './pages/app/Contacts/Contacts';
+import Leads from './pages/app/Leads/Leads';
+import Settings from './pages/app/Settings/Settings';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
+      <AppLayout>
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/leads" element={<Leads />} />
           <Route path="/settings" element={<Settings />} />
         </Routes>
-      </MainLayout>
+      </AppLayout>
     </BrowserRouter>
   )
 }
